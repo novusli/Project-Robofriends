2cedc41a9a8b027c5064ddec4a55f0bfb78fe89d
Novus Li <48393277+novusli@users.noreply.github.com>
2019-03-31 20:14:47 -0700

    Add files via upload

---
+++ src/CardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+		return (
+			<div>
+				{
+					robots.map((user,i) =>{					//user here is every element in { robots }, i is the index for every element
+						return (
+							<Card 
+							key={i} 										//key help React identify which items have changed, are added, or are removed
+							id={robots[i].id} 
+							name={robots[i].name} 
+							email={robots[i].email} 
+							username={robots[i].username} 
+							/>
+						);
+					})
+				}
+			</div>
+		);
+}
+
+export default CardList;
---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+{
+	id:1,
+	name: 'Novus Li',
+	username: 'novus',
+	email: 'novus.li@gmail.com'
+},
+{
+	id:2,
+	name: 'Edward Wang',
+	username: 'edward',
+	email: 'edward.wang@gmail.com'
+},
+{
+	id:3,
+	name: 'Jasper He',
+	username: 'jasper',
+	email: 'jasper.he@gmail.com'
+},
+{
+	id:4,
+	name: 'Jim Liu',
+	username: 'Jim',
+	email: 'jim.liu@gmail.com'
+},
+{
+	id:5,
+	name: 'Julius Wang',
+	username: 'julius',
+	email: 'julius.wang@gmail.com'
+},
+{
+	id:6,
+	name: 'Long Ma',
+	username: 'long',
+	email: 'long.ma@gmail.com'
+},
+{
+	id:7,
+	name: 'Enosh Yang',
+	username: 'enosh',
+	email: 'enosh.yang@gmail.com'
+}
+]
