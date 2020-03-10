0bb710d56ced5e16ee6cba3617fb76ec7e0cc319
novusli <novuslzq@gmail.com>
2020-03-09 17:58:06 -0700

    Added Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,53 @@
-import React, {Component} from 'react';
-// import {robots} from './robots';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField, requestRobots } from '../actions'
+
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error		
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {							//state is similar with props
-			robots: [],						//robots is from { robots } in robots.js
-			searchfield: ''
-		};
-	}
-
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => this.setState({robots: users}))
+		// console.log(this.props.store.getState())
+		// fetch('https://jsonplaceholder.typicode.com/users')
+		// .then(response => response.json())
+		// .then(users => this.setState({robots: users}))
+		this.props.onRequestRobots();
 	}
 
 	// function inside of a component, use arrow function so that 'this' may refer to 'App' instead of the function itself
-	onSearchChange=(event)=> {
-		this.setState({searchfield: event.target.value})
-	}
-
 	
 	render() {
-		const { robots, searchfield } = this.state;
+		const { searchField, onSearchChange, robots, isPending } = this.props;
 		// includes () determines if a string contains the characters of a specific string.
 		const filteredRobots = robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 
-		return !robots.length?
+		return isPending?
 			<h1>Loading</h1>:
 		(
 			<div className='tc'>
 				<h1 className='f1'>Search Your Robot</h1>
-				<SearchBox searchChange={this.onSearchChange} />
+				<SearchBox searchChange={onSearchChange} />
 				<Scroll>
 					<ErrorBoundry>
 						<CardList robots={ filteredRobots } />
@@ -52,4 +59,7 @@ class App extends Component {
 	
 }
 
-export default App;
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import App from './containers/App';
+import { searchRobots, requestRobots } from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore( rootReducer, applyMiddleware(thunkMiddleware, logger) )
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+    // Provider API makes the store available to the App
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+    )
 
 registerServiceWorker();
 // ServiceWorker.unregister();
